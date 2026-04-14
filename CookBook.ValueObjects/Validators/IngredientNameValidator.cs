826c4c3922754d3424e14a0e05b628bb16086eac
ireneshulueva <ariwookim@gmail.com>
2026-04-15 00:19:33 +0700

    Add files via upload

---
+++ CookBook.ValueObjects/Validators/IngredientNameValidator.cs
@@ -0,0 +1,20 @@
+﻿using CookBook.ValueObjects.Base;
+using CookBook.ValueObjects.Exceptions;
+
+namespace CookBook.ValueObjects.Validators
+{
+    public class IngredientNameValidator : IValidator<string>
+    {
+        public void Validate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullOrWhiteSpaceException(nameof(IngredientName));
+
+            if (value.Length < 2)
+                throw new ArgumentShortValueException(nameof(IngredientName), 2, value.Length);
+
+            if (value.Length > 100)
+                throw new ArgumentLongValueException(nameof(IngredientName), 100, value.Length);
+        }
+    }
+}
---
+++ CookBook.ValueObjects/Validators/RecipeTitleValidator.cs
@@ -0,0 +1,20 @@
+﻿using CookBook.ValueObjects.Base;
+using CookBook.ValueObjects.Exceptions;
+
+namespace CookBook.ValueObjects.Validators
+{
+    public class RecipeTitleValidator : IValidator<string>
+    {
+        public void Validate(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullOrWhiteSpaceException(nameof(RecipeTitle));
+
+            if (value.Length < 3)
+                throw new ArgumentShortValueException(nameof(RecipeTitle), 3, value.Length);
+
+            if (value.Length > 100)
+                throw new ArgumentLongValueException(nameof(RecipeTitle), 100, value.Length);
+        }
+    }
+}
