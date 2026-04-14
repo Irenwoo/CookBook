77986ea0de9755bd68ac5a46baae6584075313db
ireneshulueva <ariwookim@gmail.com>
2026-04-15 00:16:19 +0700

    Add files via upload

---
+++ Domain.Repositories.Abstractions/IIngredientRepository.cs
@@ -0,0 +1,10 @@
+﻿using CookBook.Domain;
+using CookBook.Domain.Repositories.Abstractions.Base;
+using Domain.Repositories.Abstractions.Base;
+
+namespace CookBook.Domain.Repositories.Abstractions.Base
+{
+    public interface IIngredientRepository : IRepository<Ingredient>
+    {
+    }
+}
