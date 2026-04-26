b2af8e615972f3b39a0f41bf25b39c14a21acbe7
ireneshulueva <ariwookim@gmail.com>
2026-04-26 15:38:20 +0700

    Delete CookBook directory

--- CookBook/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using CookBook.Domain;
-using CookBook.ValueObjects;
-
-// Создаём Username и Chef
-var username = new Username("GordonRamsay");
-var chef = new Chef(username);  
-
-Console.WriteLine($"Шеф: {chef.Username.Value}");  
-Console.WriteLine($"ID: {chef.Id}");
